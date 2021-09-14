ecf675612a6c1f07004bf38d838557ac0ce56201
sparrow12345 <89451813+sparrow12345@users.noreply.github.com>
2021-09-14 23:41:25 +0300

    Update ex4.c

--- week04/ex4.c
+++ week04/ex4.c
@@ -8,19 +8,23 @@
 int main()
 {
 	char a[101];
-	char *p[20];
-	while(1){
+	char *p[40];
+	while(1)
+	{
 		fgets(a,100,stdin);
 		char *tok= strtok(a," ");
 		int i=0;
-		while(tok != NULL){
+		while(tok != NULL)
+		{
 			p[i] = tok;
 			tok = strtok(NULL," ");
 			i++;
 		}
 		p[i]=NULL;
 		if(i>0)
-			p[i-1][len(p[i-1])-1] = '\n';
+		{	
+			p[i-1][strlen(p[i-1])-1] = '\0';
+		}
 		pid_t x = fork();
 		if( x == 0 ){
 				execvp(p[0],p);
